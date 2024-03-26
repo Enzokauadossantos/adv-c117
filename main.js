4404842c3d3a76f17344efc32f0991433dd9a1ef
Enzokauadossantos <157314239+Enzokauadossantos@users.noreply.github.com>
2024-03-26 15:48:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 function startClassification()
 {
   navigator.mediaDevices.getUserMedia({ audio: true});
-    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/bkaDrKcBl/model.json', modelReady);
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/y9eCjyra-/model.json', modelReady);
 }
 
 function modelReady(){
-  classifier.classify( gotResults);
+  classifier.classify(gotResults);
 }
+var dog = 0;
+var cat = 0;
 
 function gotResults(error, results) {
   if (error) {
@@ -17,36 +19,22 @@ function gotResults(error, results) {
     random_number_g = Math.floor(Math.random() * 255) + 1;
     random_number_b = Math.floor(Math.random() * 255) + 1;
 
-    document.getElementById("result_label").innerHTML = 'Posso ouvir - '+ results[0].label;
-    document.getElementById("result_confidence").innerHTML = 'Precisão - '+ (results[0].confidence*100).toFixed(2)+" %";
+
+    document.getElementById("result_label").innerHTML = 'Som detectado é de um: '+ results[0].label;
+    document.getElementById("result_count").innerHTML = 'Cachorro detectado: '+dog+ ' Gato detectado: '+cat;
     document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
-    document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
 
-    img = document.getElementById('alien1') 
-    img1 = document.getElementById('alien2')
-    img2 = document.getElementById('alien3')
-    img3 = document.getElementById('alien4')
+    img = document.getElementById('animal_image');
 
-    if (results[0].label == "Palmas") {
-      img.src = 'aliens-01.gif';
-      img1.src = 'aliens-02.png';
-      img2.src = 'aliens-03.png';
-      img3.src = 'aliens-04.png';
-    } else if (results[0].label == "Sino") {
-      img.src = 'aliens-01.png';
-      img1.src = 'aliens-02.gif';
-      img2.src = 'aliens-03.png';
-      img3.src = 'aliens-04.png';
-    } else if (results[0].label == "Estalos") {
-      img.src = 'aliens-01.png';
-      img1.src = 'aliens-02.png';
-      img2.src = 'aliens-03.gif';
-      img3.src = 'aliens-04.png';
-    }else{
-      img.src = 'aliens-01.png';
-      img1.src = 'aliens-02.png';
-      img2.src = 'aliens-03.png';
-      img3.src = 'aliens-04.gif';
+    if (results[0].label == "latido") {
+      img.src = 'bark.gif';
+      dog = dog+1;
+    } else if (results[0].label == "miado") {
+      img.src = 'meow.gif';
+      cat = cat + 1;
+    } else{
+      img.src = 'listen.gif';
     }
   }
 }
